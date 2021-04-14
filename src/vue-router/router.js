647f586b1ad9cb0bf52582e48c787e09fe4bda86
Ruslan Kumitskiy <ruslankum2@gmail.com>
2021-04-14 18:00:16 +0300

    vue-router changed from history mode to hash

--- src/vue-router/router.js
+++ src/vue-router/router.js
@@ -8,7 +8,7 @@ import Yellow from '../components/Yellow.vue'
 Vue.use(Router)
 
 export default new Router({
-    mode: 'history',
+    // mode: 'history',
     routes: [
         {
             path: '/',
